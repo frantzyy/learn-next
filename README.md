67af720113ec715965569464499e799ca3a0571f
Chris Frantz <chrisfrantz@Chriss-MacBook-Air.local>
2018-11-22 09:18:44 -0500

    just finished clear url lesson

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Learning about Next.js 
+
+This repo is following the lessons from here: https://nextjs.org/learn/
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/MyLayout.js'
+
+export default () => (
+    <Layout>
+       <p>About: this is the About page</p>
+    </Layout>
+)
