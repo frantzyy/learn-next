3f35ab04510b0d1ec9bcaab91403b829158be8b7
Chris Frantz <chrisfrantz@Chriss-MacBook-Air.local>
2018-11-22 09:32:24 -0500

    added batman lookup

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ app.prepare()
 .then(() => {
   const server = express()
 
+  // server.get('/p/:id', (req, res) => {
+  //   const actualPage = '/post'
+  //   const queryParams = { title: req.params.id } 
+  //   app.render(req, res, actualPage, queryParams)
+  // })
+
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
+    const queryParams = { id: req.params.id }
     app.render(req, res, actualPage, queryParams)
-  })
+})
 
 
   server.get('*', (req, res) => {
